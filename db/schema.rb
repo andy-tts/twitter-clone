aafd4ab47b75f4b5e647b5269bff95d4e533b1f5
Andy TTS <awhillans@techtalentsouth.com>
2017-11-09 09:53:32 -0500

    Add some new columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109143348) do
+ActiveRecord::Schema.define(version: 20171109144716) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(version: 20171109143348) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_salt"
+    t.string "name"
+    t.string "username"
+    t.text "bio"
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
