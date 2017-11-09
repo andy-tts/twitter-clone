448be093e4fe65c8d68c7b704ed4c9696c968a07
Andy TTS <awhillans@techtalentsouth.com>
2017-11-09 11:01:26 -0500

    Add some validation to hoots and users

--- app/models/hoot.rb
+++ app/models/hoot.rb
@@ -1,3 +1,9 @@
 class Hoot < ApplicationRecord
 	belongs_to :user
+
+	validates :message, presence: true, 
+		length: { 
+			maximum: 250, 
+			too_long: 'Hoots must be less than 250 characters!' 
+		}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :hoots
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
 end
