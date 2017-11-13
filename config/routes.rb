d2244123af2fe59d9fc6a23bbb0ec692ff27b21a
Andy TTS <awhillans@techtalentsouth.com>
2017-11-13 09:44:17 -0500

    Create epicenter controller and some views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'epicenter/feed'
+
+  get 'epicenter/show_user'
+
+  get 'epicenter/now_following'
+
+  get 'epicenter/unfollow'
+
   resources :hoots
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'hoots#index'
+  root 'epicenter#feed'
 end
