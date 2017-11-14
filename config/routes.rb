6c371455d57aa83c9b58f73f37ed8d9ff3a1ad19
Andy TTS <awhillans@techtalentsouth.com>
2017-11-14 08:19:14 -0500

    Add unfollow link

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   get 'epicenter/unfollow'
 
+  get 'unfollow' => 'epicenter#unfollow'
+
   resources :hoots
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
