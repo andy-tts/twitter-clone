630b907924409b129eda961e2457ed53a1433aa0
Andy TTS <awhillans@techtalentsouth.com>
2017-11-14 10:13:58 -0500

    Linkify tags within hoots

--- app/controllers/hoots_controller.rb
+++ app/controllers/hoots_controller.rb
@@ -25,6 +25,28 @@ class HootsController < ApplicationController
   # POST /hoots.json
   def create
     @hoot = Hoot.new(hoot_params)
+    # the quick brown #typing #skills
+    message_array = @hoot.message.split(' ')
+    # ['the', 'quick', 'brown', '#typing', '#skills']
+
+    message_array.each_with_index do |word, index|
+      if word[0] == "#"
+        existing_tags = Tag.pluck(:phrase)
+        if existing_tags.include?(word)
+          tag = Tag.find_by(phrase: word)
+        else
+          tag = Tag.create(phrase: word)
+        end
+
+        hoot_tag = HootTag.create(tag: tag, hoot: @hoot)
+
+        message_array[index] = "<a href='/tag_hoots?id=#{tag.id}'>#{word}</a>"
+      end
+    end
+    # ['the', 'quick', 'brown', '<a href="/">#typing</a>', '<a href="/">#skills</a>']
+    updated_message = message_array.join(' ')
+    # the quick brown <a href="/">#typing</a> <a href="/"> #skills</a>
+    @hoot.update(message: updated_message)
 
     respond_to do |format|
       if @hoot.save
